cba98c0e65e0a4ce4c4cc2766091123826fde57f
Pablo Rocha <pablorochatmm@gmail.com>
2022-05-28 22:40:15 -0500

    decieded to go for another ui path

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,32 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+	//Possible fun facts
+	const funFacts = [
+		"Avocados are a fruit, not a vegetable.",
+		"The Eiffel Tower can be 15 cm taller during the summer",
+		"The heart of a shrimp is located in its head.",
+		"Baby rabbits are called kits",
+		"The Easter Island heads have bodies.",
+		"Ketchup was once sold as medicine.",
+	];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+	// Pick a random fun fact
+	const greeting = funFacts[Math.floor(Math.random() * funFacts.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+	// Add it to the page.
+	const greetingContainer = document.getElementById("name");
+	greetingContainer.innerText = greeting;
+	//Make animation happen again
+	greetingContainer.style.animation = "none";
+	greetingContainer.offsetHeight;
+	greetingContainer.style.animation = null;
 }
+/**
+ * Navigates to another url.
+ */
+
+const navigate = (url) => {
+	window.location.href = url;
+};
