095a63ec9e4f099155a2d58fcb61bd48297b924e
Pablo Rocha <pablorochatmm@gmail.com>
2022-06-06 23:54:41 -0500

    fix: :ambulance: Correct class name in fun fact java

--- portfolio/src/main/java/com/google/sps/servlets/FunFactServerlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FunFactServerlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/funFact")
-public class FunFact extends HttpServlet {
+public class FunFactServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
