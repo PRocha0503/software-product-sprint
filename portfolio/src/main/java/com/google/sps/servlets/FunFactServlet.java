51a1c9cf7872426a9ce218c38cea88fac396bb57
Pablo Rocha <pablorochatmm@gmail.com>
2022-06-08 00:29:16 +0000

    Random mesage from server

--- portfolio/src/main/java/com/google/sps/servlets/FunFactServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FunFactServlet.java
@@ -6,15 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.Random;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/funFact")
 public class FunFactServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String data = "This is a fun fact";
+    String[] funFacts = {"Avocados are a fruit, not a vegetable.",
+		"The Eiffel Tower can be 15 cm taller during the summer",
+		"The heart of a shrimp is located in its head.",
+		"Baby rabbits are called kits",
+		"The Easter Island heads have bodies.",
+		"Ketchup was once sold as medicine."};
+	Random generator = new Random();
+    int randomIndex = generator.nextInt(funFacts.length);
+    String selectedFact = funFacts[randomIndex];
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
-    response.getWriter().write(data);
+    response.getWriter().write(selectedFact);
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,35 +1,17 @@
 /**
- * Adds a random greeting to the page.
- */
-function addRandomFact() {
-	//Possible fun facts
-	const funFacts = [
-		"Avocados are a fruit, not a vegetable.",
-		"The Eiffel Tower can be 15 cm taller during the summer",
-		"The heart of a shrimp is located in its head.",
-		"Baby rabbits are called kits",
-		"The Easter Island heads have bodies.",
-		"Ketchup was once sold as medicine.",
-	];
-
-	// Pick a random fun fact
-	const selectedFunFact = funFacts[Math.floor(Math.random() * funFacts.length)];
-
-	// Add it to the page.
-	const funFactContainer = document.getElementById("name");
-	funFactContainer.innerText = selectedFunFact;
-	//Make animation happen again
-	funFactContainer.style.animation = "none";
-	funFactContainer.offsetHeight;
-	funFactContainer.style.animation = null;
-}
-
-/**
  * Gets a random fact from the server
  */
 const getRandomFact = async () => {
 	const serverFunFact = await fetch("/funFact");
-	console.log(serverFunFact.text());
+	const textFunFact = await serverFunFact.text()
+    // Add it to the page.
+	const funFactContainer = document.getElementById("name");
+	funFactContainer.innerText = textFunFact;
+	//Make animation happen again
+	funFactContainer.style.animation = "none";
+	funFactContainer.offsetHeight;
+	funFactContainer.style.animation = null;
+
 };
 
 /**
