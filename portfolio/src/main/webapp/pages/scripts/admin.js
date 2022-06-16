5e27f9b8075ccbce2a7acdf7f72bf3b6ec410abe
Pablo Rocha <pablorochatmm@gmail.com>
2022-06-16 17:18:20 -0500

    fix: :bug: Left dummy data

--- portfolio/src/main/webapp/pages/scripts/admin.js
+++ portfolio/src/main/webapp/pages/scripts/admin.js
@@ -3,27 +3,27 @@ const main = () => {
 };
 
 const addRows = async () => {
-	// const response = await fetch("/getContacts");
-	// const json = await response.text();
-	// const contacts = JSON.parse(json);
-	const contacts = [
-		{
-			name: "Test",
-			email: "test@example.com",
-			reason: "TEST",
-			recruiter: false,
-			active: true,
-			time: "1213213",
-		},
-		{
-			name: "Test",
-			email: "test@example.com",
-			reason: "TEST",
-			recruiter: false,
-			active: true,
-			time: "1213213",
-		},
-	];
+	const response = await fetch("/getContacts");
+	const json = await response.text();
+	const contacts = JSON.parse(json);
+	// const contacts = [
+	// 	{
+	// 		name: "Test",
+	// 		email: "test@example.com",
+	// 		reason: "TEST",
+	// 		recruiter: false,
+	// 		active: true,
+	// 		time: "1213213",
+	// 	},
+	// 	{
+	// 		name: "Test",
+	// 		email: "test@example.com",
+	// 		reason: "TEST",
+	// 		recruiter: false,
+	// 		active: true,
+	// 		time: "1213213",
+	// 	},
+	// ];
 	let table = document.getElementById("contactsTable");
 	contacts.map((ob, i) => {
 		let row = table.insertRow(i + 1);
